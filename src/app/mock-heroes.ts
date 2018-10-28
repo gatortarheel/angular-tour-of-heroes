993d8d7f325f0ec7af35fe6020c3bba56d0f7c73
gatortarheel <gatortarheel@gmail.com>
2018-10-28 17:21:21 -0400

    update

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Captain America', strength: 16 },
+    { id: 12, name: 'Batman', strength: 16 },
+    { id: 13, name: 'Carnage', strength: 16 },
+    { id: 14, name: 'Deadpool', strength: 15 },
+    { id: 15, name: 'Electro', strength: 14 },
+    { id: 16, name: 'Flash', strength: 14 },
+    { id: 17, name: 'Green Lantern', strength: 10 },
+    { id: 18, name: 'Hulk', strength: 18 },
+    { id: 19, name: 'Iceman', strength: 14 },
+    { id: 20, name: 'Joker', strength: 12 }
+  ];
